2f33cd276c79c118921561c7bfeb7defedcc58d4
Vidit <viditmodi2001@gmail.com>
2023-10-24 14:28:31 +0530

    added netlify origin to backend

--- Level 3 - Blogging Platform/server/index.js
+++ Level 3 - Blogging Platform/server/index.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.PORT;
 
 const corsOptions = {
-    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5501"],
+    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5501", "https://quillcraft.netlify.app"],
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, authorization",
     methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
