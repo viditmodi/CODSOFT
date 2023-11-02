c8130064fcf20fb6edad44e16ca37a957cc3a01b
Vidit <viditmodi2001@gmail.com>
2023-11-02 06:38:17 +0530

    blog is now completed, save for testing

--- Level 3 - Blogging Platform/client/js/data/apiURLs.js
+++ Level 3 - Blogging Platform/client/js/data/apiURLs.js
@@ -1,5 +1,5 @@
-const baseURL = "http://localhost:3100"
-// const baseURL = "https://quillcraft-api.onrender.com"
+// const baseURL = "http://localhost:3100"
+const baseURL = "https://quillcraft-api.onrender.com"
 
 
 
