8430c8f2597f9c566cf40f5be5c01ab33c446462
viditmodi <123234937+viditmodi@users.noreply.github.com>
2023-11-11 15:00:19 +0530

    Update header.js - searchBar()

--- Level 3 - Blogging Platform/client/templates/header.js
+++ Level 3 - Blogging Platform/client/templates/header.js
@@ -1,33 +1,33 @@
-// const searchBar = (data) => {
-//   if (/^\s*$/.test(data)) {
-//     return alert("enter a valid string");
-//   }
-//   let searchQuery, searchType;
-//   data = data.split(":");
-//   if (data.length < 2) {
-//     searchType = "post";
-//     searchQuery = data[0];
-//   } else {
-//     searchType = data[0];
-//     searchQuery = data[1];
-//   }
-//   console.log(searchType, searchQuery);
+const searchBar = (data) => {
+  if (/^\s*$/.test(data)) {
+    return alert("enter a valid string");
+  }
+  let searchQuery, searchType;
+  data = data.split(":");
+  if (data.length < 2) {
+    searchType = "post";
+    searchQuery = data[0];
+  } else {
+    searchType = data[0];
+    searchQuery = data[1];
+  }
+  console.log(searchType, searchQuery);
 
-//   searchType = searchType.toLowerCase()
-//   searchQuery = searchQuery.toLowerCase()
+  searchType = searchType.toLowerCase()
+  searchQuery = searchQuery.toLowerCase()
 
-//   if(!["category", "blog", "author", "post"].includes(searchType)){
-//     return alert("invalid search param")
-//   }
+  if(!["category", "blog", "author", "post"].includes(searchType)){
+    return alert("invalid search param")
+  }
 
-//   searchQuery = searchQuery
-//     .split(" ")
-//     .filter((ele) => ele !== "")
-//     .join(" ");
+  searchQuery = searchQuery
+    .split(" ")
+    .filter((ele) => ele !== "")
+    .join(" ");
 
 
-//   window.location.href = `/pages/search.html?t=${searchType}&q=${searchQuery}`
-// };
+  window.location.href = `/pages/search.html?t=${searchType}&q=${searchQuery}`
+};
 
 const createHeader = (parentElement, data) => {
   //orange-f// Logo
